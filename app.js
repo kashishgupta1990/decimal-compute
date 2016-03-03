e79e82fd8d098fe4068018e27396eee6582acdb8
Kashish Gupta <kashishgupta1990@yahoo.com>
2016-03-03 14:05:16 +0530

    added name and remove field

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var dataSource = require('./dataSource.json');
 var durationOutputResult = '';
 var timeOutputResult = '';
 
-Number.prototype.round = function(decimals) {
-    return Number((Math.round(this + "e" + decimals)  + "e-" + decimals));
+Number.prototype.round = function (decimals) {
+    return Number((Math.round(this + "e" + decimals) + "e-" + decimals));
 };
 
 var writeData = function (fileName, dataToWrite) {
@@ -14,33 +14,58 @@ var writeData = function (fileName, dataToWrite) {
     });
 };
 
-var computeDurationBlock = function(){
+Number.prototype.fixThreeDecimalPlace = function () {
+    var strNumber = ('' + this);
+    if(!strNumber.match(/\./g)){
+        return strNumber + '.000';
+    }
+    var splitObj = strNumber.split('.');
+
+    var decimalFigure = splitObj[1];
+    var newDecimalFigure = '';
+    var lenFigure = decimalFigure.length;
+
+    switch (lenFigure) {
+        case 1:
+            newDecimalFigure = decimalFigure + '00';
+            break;
+        case 2:
+            newDecimalFigure = decimalFigure + '0';
+            break;
+        default:
+            newDecimalFigure = decimalFigure
+    }
+
+    return splitObj[0]+'.'+newDecimalFigure;
+};
+
+var computeDurationBlock = function () {
     var durationF1 = '0.000';
     var durationF2 = '';
     var durationResult = '0.000';
-    dataSource.words.forEach(function(data){
+    dataSource.words.forEach(function (data) {
         durationF2 = (+data.duration).round(3);
         durationResult = +(+durationF1).round(3) + (+durationF2).round(3);
-        durationOutputResult+=''+(+durationF1).round(3)+'  '+(+durationF2).round(3)+'  '+(+durationResult).round(3)+'\n';
+        durationOutputResult += '' + (+durationF1).round(3).fixThreeDecimalPlace() + '  ' + (+durationResult).round(3).fixThreeDecimalPlace() +'   '+ data.name + '\n';
         durationF1 = durationResult.round(3);
     });
-    writeData('duration-output.txt',durationOutputResult);
+    writeData('duration-output.txt', durationOutputResult);
 };
 
-var computeTimeBlock = function(){
+var computeTimeBlock = function () {
     var timeF1 = '0.000';
     var timeF2 = '';
     var timeResult = '0.000';
-    dataSource.words.forEach(function(data){
+    dataSource.words.forEach(function (data) {
         timeF2 = (+data.time).round(3);
         timeResult = +(+timeF1).round(3) + (+timeF2).round(3);
-        timeOutputResult+=''+(+timeF1).round(3)+'  '+(+timeF2).round(3)+'  '+(+timeResult).round(3)+'\n';
+        timeOutputResult += '' + (+timeF1).round(3).fixThreeDecimalPlace() + '  ' + (+timeResult).round(3).fixThreeDecimalPlace() +'   '+ data.name + '\n';
         timeF1 = timeResult.round(3);
     });
-    writeData('time-output.txt',timeOutputResult);
+    writeData('time-output.txt', timeOutputResult);
 };
 
-var init = function(){
+var init = function () {
     computeDurationBlock();
     computeTimeBlock();
 };
